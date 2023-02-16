7f74fa1ab179fa37c94e42e3649f5763342225d4
Grégoire <nodilod@gmail.com>
2023-02-16 20:18:15 +0100

    new discord function

--- discord.mjs
+++ discord.mjs
@@ -29,36 +29,135 @@ bot.on('messageCreate', async (msg) => {
         }
 
         if (msg.content.startsWith('!help')) {
-            await bot.createMessage(msg.channel.id, 'Commandes disponibles: !hello, !scan');
+            help(bot, msg);
         }
 
         if (msg.content.startsWith('!scan')) {
-            await bot.createMessage(msg.channel.id, 'Scanning...');
-            const messages = await main(tweet)
-            if (messages.length === 0) {
-                await bot.createMessage(msg.channel.id, "Rien à signaler");
-            } else {
-                messages.forEach(message => {
-                    bot.createMessage(msg.channel.id, message);
-                });
-            }
+            scan(bot, msg);
         }
+
+        if (msg.content.startsWith('!find')) {
+            find(bot, msg);
+        }
+
+        if (msg.content.startsWith('!history')) {
+            history(bot, msg);
+        }
+
     }
 
     // Si le message est "!random", envoyer un film en stock au hasard
     if (msg.content === '!random') {
-        const prisma = new PrismaClient();
-        const films = await prisma.film.findMany({
-            where: {
-                isInStock: true
-            }
-        });
-        await prisma.$disconnect();
-
-        const film = films[Math.floor(Math.random() * films.length)];
-        await bot.createMessage(msg.channel.id, `Film en stock au hasard: ${film.name} - ${film.url}`);
+        random(bot, msg);
     }
-
 });
 
 bot.connect();
+
+async function help(bit, msg) {
+    await bot.createMessage(msg.channel.id, 'Commandes disponibles: \n\t!hello,\n\t!scan,\n\t!random,\n\t!find <nom du film>');
+}
+
+async function scan(bot, msg) {
+    await bot.createMessage(msg.channel.id, 'Scanning...');
+    const messages = await main(tweet)
+    if (messages.length === 0) {
+        await bot.createMessage(msg.channel.id, "Rien à signaler");
+    } else {
+        messages.forEach(message => {
+            bot.createMessage(msg.channel.id, message);
+        });
+    }
+}
+
+async function find(bot, msg) {
+    const prisma = new PrismaClient();
+    // récupérer tous les films en stock avec le nom du shop
+    const films = await prisma.film.findMany({
+        where: {
+            isInStock: true
+        },
+        include: {
+            shop: true
+        }
+    });
+    await prisma.$disconnect();
+
+    const filmName = msg.content.replace('!find ', '');
+    // convert to lower case and remove accents
+    const filmNameLower = filmName.normalize("NFD").replace(/[\u0300-\u036f]/g, "").toLowerCase();
+    const foundFilms = films.filter(film =>  (film.name.normalize("NFD").replace(/[\u0300-\u036f]/g, "").toLowerCase()).includes(filmName));
+
+    if (foundFilms.length === 0) {
+        await bot.createMessage(msg.channel.id, `Aucun film ne correspond à la recherche: ${filmName}`);
+    } else {
+        // dire le nombre de films trouvés
+        await bot.createMessage(msg.channel.id, `${foundFilms.length} film(s) trouvé(s) pour la recherche: ${filmName}`);
+
+        //si trop de films trouvés, ne pas envoyer le message
+        if (foundFilms.length > 25) {
+            await bot.createMessage(msg.channel.id, `Trop de films trouvés, veuillez préciser votre recherche`);
+        } else {
+            let message = '';
+            foundFilms.forEach(film => {
+                bot.createMessage(
+                    msg.channel.id,
+                    `\n id : ${film.id} -> ${film.name} ${film.price}€ ${film.shop.name} - ${film.isInStock ? 'en stock' : 'indisponible'}\n`);
+            });
+        }
+    }
+}
+
+async function random(bot, msg) {
+    const prisma = new PrismaClient();
+    const films = await prisma.film.findMany({
+        where: {
+            isInStock: true
+        }
+    });
+    await prisma.$disconnect();
+
+    const film = films[Math.floor(Math.random() * films.length)];
+    await bot.createMessage(msg.channel.id, `Film en stock au hasard: ${film.name} - ${film.url}`);
+}
+
+async function history(bot , msg) {
+    // !history 10
+    // !history <id du film>
+
+    const filmId = parseInt(msg.content.replace('!history ', ''));
+    if (isNaN(filmId)) {
+        await bot.createMessage(msg.channel.id, `Veuillez entrer un id valide`);
+    } else {
+        const prisma = new PrismaClient();
+        const films = await prisma.film.findFirst({
+            where: {
+                id: filmId
+            }
+        })
+        await prisma.$disconnect();
+        if (films === null) {
+            await bot.createMessage(msg.channel.id, `Aucun film ne correspond à l'id: ${filmId}`);
+        } else {
+            const prisma = new PrismaClient();
+            const history = await prisma.FilmHistoryRecord.findMany({
+                where: {
+                    filmId: filmId
+                },
+                orderBy: {
+                    createdAt: 'desc'
+                }
+            })
+            await prisma.$disconnect();
+            if (history.length === 0) {
+                await bot.createMessage(msg.channel.id, `Aucun historique pour le film: ${films.name}`);
+            } else {
+                await bot.createMessage(msg.channel.id, `Historique pour le film: ${films.name}`);
+                bot.createMessage(msg.channel.id, `en stock: O|N \t prix: \t date: \n\n`);
+                history.forEach(h => {
+                    bot.createMessage(msg.channel.id, `${h.isInStock ? 'O' : 'N'}\t${h.price}\t\t${ h.createdAt.getDate()}/${h.createdAt.getMonth()}/${h.createdAt.getFullYear()}`);
+                });
+            }
+        }
+    }
+}
